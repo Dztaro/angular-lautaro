89b7bdce93e0b71745eb90300ae3f546354637a3
Lautaro Diaz <lautaroxavierdiaz@gmail.com>
2025-03-19 15:04:55 -0300

    Commir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
